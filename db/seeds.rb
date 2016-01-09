47734f3da4b56533ea654326f9218cc534077aca
Tommy Sullivan <tommy.b.sullivan@gmail.com>
2016-01-09 18:01:06 +0700

    add seeds :-)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create email: 'chrstnatalia@gmail.com', password: 'password',
+            name: 'Lia', avatar_url: 'http://k3.okccdn.com/php/load_okc_image.php/images/0x0/0x0/2/9603652731835019710.webp'
+
+User.create email: 'tommy.b.sullivan@gmail.com', password: 'password',
+            name: 'Tommy', avatar_url: 'http://k1.okccdn.com/php/load_okc_image.php/images/150x150/558x800/410x349/878x817/0/13406059829553961563.webp'
+
+
