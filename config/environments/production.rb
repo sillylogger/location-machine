2baa1abd7f8148e9fc0ac5f8cf642dae85f8b93d
Tommy Sullivan <tommy.b.sullivan@gmail.com>
2018-12-22 13:31:16 +0700

    working on appengine deploy, fixed: - ruby version - database host - default host for devise emails (TODO: likely remove, want most UI custimization in JS)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Ensure you have defined default url options in your environments files. Here
+  # is an example of default_url_options appropriate for a development environment
+  # in config/environments/development.rb:
+  config.action_mailer.default_url_options = { host: 'locationmachine.io' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
