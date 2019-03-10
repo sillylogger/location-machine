e3244b85c54f541be60d43beecb80ac3dd749d01
tri-dang <dangtoantri@gmail.com>
2019-03-10 16:19:56 +0700

    Fix: missing name in signup form
    [https://www.pivotaltracker.com/story/show/164520184]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
 
   before_action :force_canonical_host
   before_action :set_current_host
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   def authenticate_admin_user!
     authenticate_user!
