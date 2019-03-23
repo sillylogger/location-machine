6de7c3a8292aac4d507daa1da9655d23a9fe408b
tri-dang <dangtoantri@gmail.com>
2019-03-23 22:57:24 +0700

    [#164563148] Test Login with Facebook

--- spec/features/admin/pages_spec.rb
+++ spec/features/admin/pages_spec.rb
@@ -1,16 +1,7 @@
 require 'rails_helper'
 
 describe "Admin Pages" do
-
-  let(:admin) { FactoryBot.create :user, :with_admin }
-
-  before :each do
-    visit new_user_session_path
-    fill_in 'user[email]',    with: admin.email
-    fill_in 'user[password]', with: 'password'
-    click_button 'Log in'
-    wait_until { page.current_path == admin_dashboard_path }
-  end
+  include_examples 'admin login'
 
   it "lets you create new pages through active_admin" do
     # expect {
