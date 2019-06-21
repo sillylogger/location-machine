1a785e1e4c617914fb08908bf80973a1a5738b54
tri-dang <dangtoantri@gmail.com>
2019-06-21 22:00:15 +0700

    [#162537825] Show action buttons for all screen sizes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    cookies.permanent[:back_path] = params[:back_path] if params[:back_path]
     # TODO: we can add pagination here and maybe add 'see more' in search page
     @items = Item.search_for(params[:text]).limit(10)
   end
