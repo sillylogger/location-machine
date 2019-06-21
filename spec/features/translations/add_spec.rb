8e30e83a23c0f6f296d27d9849f121df8e67e998
tri-dang <dangtoantri@gmail.com>
2019-06-21 23:02:23 +0700

    [#162537825] Test

--- spec/features/translations/add_spec.rb
+++ spec/features/translations/add_spec.rb
@@ -4,7 +4,7 @@ describe 'Translation Pages' do
 
   include_examples 'admin login'
 
-  it "lets admin create new translation through active_admin and see new translation in location show page" do
+  it "lets admin create new translation through active_admin and see new translation in location new page" do
     visit admin_translations_path
     expect(page.current_path).to include(admin_translations_path)
 
@@ -12,21 +12,19 @@ describe 'Translation Pages' do
     wait_until { page.current_path.include?(new_admin_translation_path) }
 
     select      'en', from: 'i18n_backend_active_record_translation[locale]'
-    fill_in     'i18n_backend_active_record_translation[key]', with: 'lm.action.post'
-    fill_in     'i18n_backend_active_record_translation[value]', with: 'Post!'
+    fill_in     'i18n_backend_active_record_translation[key]', with: 'lm.action.create'
+    fill_in     'i18n_backend_active_record_translation[value]', with: 'Create'
 
     click_button 'Create Translation'
 
     wait_until {
-      translation = Translation.where(key: 'lm.action.post').first
+      translation = Translation.where(key: 'lm.action.create').first
       page.current_path.include?(admin_translation_path(translation))
     }
 
-    expect(page).to have_content 'Post!'
+    expect(page).to have_content 'Create'
 
-    visit root_path
-    expect(page.current_path).to include(root_path)
-
-    expect(page).to have_content 'Post!'
+    visit new_location_path
+    expect(page.find('input[name="commit"').value).to eq 'Create'
   end
 end
