460481beb456bb4b700547d099a96274a89bd688
tri-dang <dangtoantri@gmail.com>
2019-06-21 16:13:27 +0700

    [#162537825] Retouch css for search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @items = Item.search_for(params[:text])
+    # TODO: we can add pagination here and maybe add 'see more' in search page
+    @items = Item.search_for(params[:text]).limit(10)
   end
 end
