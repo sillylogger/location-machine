1136bb60f8869b0e1f23dd97eeaa1a58b578d4e4
tri-dang <dangtoantri@gmail.com>
2019-06-22 21:25:33 +0700

    [#162537825] Scroll down to load more

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,7 @@ class SearchController < ApplicationController
   def index
     @items = Item
       .for_nearests(@origin, text: params[:text])
-      .page(params[:page] || 1).per(10)
+      .page(params[:page] || 1).per(15)
 
     @total_pages = @items.total_pages
 
