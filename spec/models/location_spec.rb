129bd6a62d45ff7c06ab2be3074caa49f7d537cc
tri-dang <dangtoantri@gmail.com>
2019-06-30 13:15:05 +0700

    [#166861389] Test

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -81,4 +81,50 @@ describe Location do
     end
   end
 
+  describe "#image and #has_image?" do
+    let(:location) { FactoryBot.create(:location) }
+
+    context 'no item' do
+      it 'return nothing' do
+        expect(location.image).to be_nil
+        expect(location).not_to be_has_image
+      end
+    end
+
+    context 'has items' do
+      let!(:item_1) { FactoryBot.create(:item, location: location) }
+      let!(:item_2) { FactoryBot.create(:item, :with_image, location: location) }
+
+      it "get image of one of items" do
+        expect(location.image.attachment).to eq item_2.image.attachment
+        expect(location).to be_has_image
+      end
+    end
+  end
+
+  describe '.for_nearests' do
+    let!(:location_1) { FactoryBot.create(:location, name: 'cake of Ba Ngoai') }
+    let!(:location_2) { FactoryBot.create(:location, name: 'noodles', description: 'noodles and cake of 4P') }
+    let!(:location_3) { FactoryBot.create(:location, name: 'pizza 4P') }
+    let!(:location_4) { FactoryBot.create(:location, name: 'cake of Ba Noi') }
+    let(:latitude) { -6.2189898 }
+    let(:longitude) {106.7861758 }
+
+    before do
+      Location.update(latitude: latitude, longitude: longitude)
+      location_4.update(latitude: 0, longitude: 0)
+    end
+
+    context 'no text' do
+      it 'returns nearest locations within distance 50' do
+        expect(Location.for_nearests([latitude, longitude])).to match_array [location_1, location_2, location_3]
+      end
+    end
+
+    context 'has text' do
+      it 'returns nearest locations within distance 50 with matched text' do
+        expect(Location.for_nearests([latitude, longitude], text: 'cake')).to match_array [location_1, location_2]
+      end
+    end
+  end
 end
