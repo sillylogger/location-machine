94a13179f90e23d442887a4c58a4e8f0e0058cac
Tommy Sullivan <tommy.b.sullivan@gmail.com>
2019-07-14 17:16:10 +0700

    tidy up admin a bit

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,33 +5,35 @@ ActiveAdmin.register_page "Dashboard" do
     columns do
 
       column do
-        panel "Locations of #{Location.count}" do
+        panel "Most recent Locations of #{Location.count}" do
           ul(class: 'compact') do
             Location.order(id: :desc).limit(5).map do |location|
               summary = "#{location.name} @ #{location.latitude}, #{location.longitude}"
               li link_to(summary, admin_location_path(location))
             end
           end
+          a("See all Locations", href: admin_locations_path, class: "see-all")
         end
 
-        panel "Users of #{User.count}" do
+        panel "Most recent Users of #{User.count}" do
           ul(class: 'compact') do
             User.order(id: :desc).limit(5).map do |user|
               summary = "#{user.name} #{user.email} #{user.phone}"
               li link_to(summary, admin_user_path(user))
             end
           end
+          a("See all Users", href: admin_users_path, class: "see-all")
         end
       end
 
       column do
-        panel "Items of #{Item.count}" do
-          ul(class: 'compact') do
+        panel "Most recent Items of #{Item.count}" do
+          ul(class: 'compact items') do
             Item.order(id: :desc).limit(5).map do |item|
-              summary = "#{item.name} #{number_to_currency(item.price)}"
-              li link_to(summary, admin_item_path(item))
+              render 'item', item: item 
             end
           end
+          a("See all Items", href: admin_items_path, class: "see-all")
         end
       end
 
