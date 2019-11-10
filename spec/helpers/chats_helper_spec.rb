6a311e875ffce9cc36a6cc25f1ce19a2e944e287
Tommy Sullivan <tommy.b.sullivan@gmail.com>
2019-11-10 22:40:20 +0700

    [#167668035] simplify the test for readability

--- spec/helpers/chats_helper_spec.rb
+++ spec/helpers/chats_helper_spec.rb
@@ -3,37 +3,25 @@ require 'rails_helper'
 describe ChatsHelper do
 
   describe "#native_chat_url" do
-    let(:urls) {
-      {
-        'facebook' => 'https://m.me/',
-        'whatsapp' => 'https://wa.me/',
-        'zalo' => 'https://zalo.me/'
-      }
-    }
     it "returns correct url" do
-      expected = '123'
-
-      urls.each do |k,v|
-        expect(helper.native_chat_url(k, expected)).to eq "#{v}#{expected}"
-      end
+      expect(helper.native_chat_url('facebook', '123')).to eq 'https://m.me/123'
+      expect(helper.native_chat_url('whatsapp', '123')).to eq 'https://wa.me/123'
+      expect(helper.native_chat_url('zalo', '123')).to     eq 'https://zalo.me/123'
     end
 
-    it "strips any non-number characters" do
-      data = [%w(+00123 123), %w(+62123 62123),
-              %w(+621-2-3 62123), %w(+62\ 1\ 2\ 3 62123)]
-      urls.each do |k, v|
-        data.each do |item|
-          input, output = item
-          expect(helper.native_chat_url(k, input)).to eq "#{v}#{output}"
-        end
-      end
+    it "strips any zeroes, brackets or dashes" do
+      expect(helper.native_chat_url('whatsapp', '+00123')).to   eq 'https://wa.me/123'
+      expect(helper.native_chat_url('whatsapp', '+62123')).to   eq 'https://wa.me/62123'
+      expect(helper.native_chat_url('whatsapp', '+621-2-3')).to eq 'https://wa.me/62123'
+      expect(helper.native_chat_url('whatsapp', '+621 2 3')).to eq 'https://wa.me/62123'
     end
 
     context 'whatsapp' do
-      it "adds the regarding text if supplied" do
+      it "adds the regarding text if supplied" do 
         expect(helper.native_chat_url('whatsapp', '+00123', regarding: 'foo bar')).to include('text=')
         expect(helper.native_chat_url('whatsapp', '+00123', regarding: 'foo bar')).to include('foo%20bar')
       end
     end
   end
+
 end
